b7ea8b0d39d6961c8de77cfefac7b43483f73252
bárbara-valdez-zaidan <babivaldez@gmail.com>
2022-10-01 08:22:01 -0300

    Implementei a função createMenu, bem como seus casos de teste

--- src/restaurant.js
+++ src/restaurant.js
@@ -93,6 +93,130 @@
 // - retornará o valor somado acrescido de 10%.
 // DICA: para isso, você precisará percorrer tanto o objeto da chave `food` quanto o objeto da chave `drink`.
 
-const createMenu = () => {};
+// ---------------------------------------------------------------------------------------------------------------------
+// INICIO DA RESOLUÇÃO 
+
+const menu = { food: { coxinha: 3.9, sopa: 9.9 }, drink: { agua: 3.9, cerveja: 6.9 } };
+
+const restaurant = {};
+
+const orderFromMenu = (request) => {
+  restaurant.consumption.push(request);
+};
+
+// Eu tinha feito apenas como está abaixo, só que dava erro NaN, pois quando tinha uma chave de drink, eu tentava acessar direto no menu de comida e não dava certo. Era preciso ter uma comparação antes.
+// --------------------------------------------------------------------------------------------------------------------------------------
+// const totalFood = () => {
+//   let somaFood = 0;
+//   const menuFood = restaurant.fetchMenu().food;
+//   const consumo = restaurant.consumption; 
+//   for (let index = 0; index < restaurant.consumption.length; index += 1) {  
+//     somaFood += menuFood[consumo];
+//   }
+//   return somaFood;
+// };
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+// Essa comparação também não deu certo, pois o que eu estava comparando era, por exemplo, coxinha === menuFood.coxinha, ou seja, o nome da chave com o resultado dela, que é um número. Então sempre retornava 0. 
+// ----------------------------------------------------------------------------------------------------------------------------------------
+// const totalFood = () => {
+//   let somaFood = 0;
+//   const menuFood = restaurant.fetchMenu().food;
+//   const consumo = restaurant.consumption; 
+//   for (let index = 0; index < restaurant.consumption.length; index += 1) {
+//     if (consumo[index] === menuFood[consumo[index]]) {
+//     somaFood += menuFood[consumo[index]];
+//     }
+//   }
+//   return somaFood;
+// };
+// ----------------------------------------------------------------------------------------------------------------------------------------
+
+// Eu tentei usar o código abaixo e funciona, mas o lint reclama dizendo que não se deve usar o método Object.prototype 'hasOwnProperty' do objeto de destino.
+// --------------------------------------------------------------------------------------------------------------------------------------
+// const totalFood = () => {
+//   let somaFood = 0;
+//   const menuFood = restaurant.fetchMenu().food;
+//   for (let index = 0; index < restaurant.consumption.length; index += 1) {
+//     if (menuFood.hasOwnProperty([restaurant.consumption[index]])) {
+//     somaFood += menuFood[restaurant.consumption[index]];
+//     }
+//   }
+//   return somaFood;
+// };
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+// No código abaixo, o IN serve para verificar se tal chave existe no objeto a seguir. Ele retorna um booleano.
+const totalFood = () => {
+  let somaFood = 0;
+  const menuFood = restaurant.fetchMenu().food;
+  for (let index = 0; index < restaurant.consumption.length; index += 1) {
+    if (restaurant.consumption[index] in menuFood) {
+    somaFood += menuFood[restaurant.consumption[index]];
+    }
+  }
+  return somaFood;
+};
+
+// O código abaixo funciona, mas o Lint reclamou da complexidade dele, dizendo para eu reduzir de 6 para 5. Então precisei refatorar com os vários códigos acima.
+// ---------------------------------------------------------------------------------------------------------------------------------------
+// const totalDrink = () => {
+//   let somaDrink = 0;
+//   const drink = Object.entries(menu.drink);
+//   for (let index = 0; index < restaurant.consumption.length; index += 1) {
+//     for (let index1 = 0; index1 < drink.length; index1 += 1) {
+//       if (restaurant.consumption[index] === drink[index1][0]) {
+//         somaDrink += drink[index1][1];
+//       }
+//     }
+//   }
+//   return somaDrink;
+// };
+// ---------------------------------------------------------------------------------------------------------------------------------------
+
+const totalDrink = () => {
+  let somaDrink = 0;
+  const menuDrink = restaurant.fetchMenu().drink;
+  for (let index = 0; index < restaurant.consumption.length; index += 1) {
+    if (restaurant.consumption[index] in menuDrink) {
+      somaDrink += menuDrink[restaurant.consumption[index]];
+    }
+  }
+  return somaDrink;
+};
+
+const totalValor = () => {
+  let soma = 0;
+  let somaComGorgeta = 0;
+  soma = (totalFood() + totalDrink());
+  somaComGorgeta = soma + soma * 0.1;
+  return somaComGorgeta;
+};
+
+const createMenu = (objetoMenu) => {
+  const retornaMenu = () => objetoMenu; // o lint reclamou quando coloquei a função direto no fetchMenu
+
+  restaurant.fetchMenu = retornaMenu;
+  restaurant.consumption = [];
+  restaurant.order = orderFromMenu;
+  restaurant.pay = totalValor;
+  
+  return restaurant;
+};
+
+createMenu(menu);
+
+restaurant.order('coxinha');
+restaurant.order('agua');
+restaurant.order('coxinha');
+
+// console.log(Object.entries(menu.drink));
+// console.log(Object.entries(menu.food));
+// console.log (totalDrink());
+// console.log(totalFood());
+// console.log(restaurant.fetchMenu());
+// console.log(restaurant.consumption;
+// console.log(restaurant.order('coxinha'));
+// console.log(restaurant.pay());
 
 module.exports = createMenu;
--- tests/restaurant.spec.js
+++ tests/restaurant.spec.js
@@ -53,79 +53,108 @@ const createMenu = require('../src/restaurant');
 */
 
 describe('10 - Implemente a função `createMenu`, bem como seus casos de teste', () => {
-  it('Verifica se a função `createMenu` tem o comportamento esperado', () => {
-    fail('Teste vazio!');
-    // TESTE 1: Verifique se função `createMenu()` retorna um objeto que possui a chave `fetchMenu`, a qual tem como valor uma função.
-    // ```
-    // const objetoRetornado = createMenu(); // Retorno: { fetchMenu: () => {}, ... }
-    // ```
-    // TESTE 2: Verifique se 'objetoRetornado.fetchMenu()' retorna um objeto cujas chaves são somente `food` e `drink`, 
-    // considerando que a função createMenu() foi chamada com o objeto: `{ food: {}, drink: {} }`.
-    // ```
-    // const objetoRetornado = createMenu({ food: {}, drink: {} });
-    // objetoRetornado.fetchMenu() // Retorno: { food: {}, drink: {}}
-    // ```
-    // TESTE 3: Verifique se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função 'objetoRetornado.fetchMenu()'
-    // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
-    // objetoRetornado.fetchMenu() // Retorno: objetoQualquer
-    // ```
+  it("Verifica se função 'createMenu()' retorna um objeto que possui a chave 'fetchMenu', a qual tem como valor uma função.", () => {
+      
+    const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };
+    
+    expect(createMenu(menu)).toHaveProperty('fetchMenu');
+    
+  });
+
+  it("Verifica se objetoRetornado.fetchMenu() retorna um objeto cujas chaves são somente 'food' e 'drink', considerando que a função createMenu() foi chamada com o objeto: '{ food: {}, drink: {} }'", () => {
+    
+    const objeto = createMenu({ food: {}, drink: {} });
+    
+    expect(objeto.fetchMenu()).toHaveProperty('food');
+    expect(objeto.fetchMenu()).toHaveProperty('drink');
+    
+  });
+    
+  it("Verifica se o menu passado pra função createMenu() é idêntico ao menu recuperado pela função 'objetoRetornado.fetchMenu()'", () => {
+
+    const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };  
+    const objetoRetornado = createMenu(menu);
+    
+    expect(objetoRetornado.fetchMenu()).toEqual(menu);
+    
+    });
 
     // Agora faça o PASSO 1 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
 
-    // TESTE 4: Verifique se 'objetoRetornado.consumption', após a criação do menu, retorna um array vazio.
-    // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
-    // objetoRetornado.consumption // Retorno: []
-    // ```
+    it("Verifica se objetoRetornado.consumption após a criação do menu, retorna um array vazio.", () => {
+  
+      const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };  
+      const objetoRetornado = createMenu(menu);
+
+      expect(objetoRetornado.consumption).toEqual([]);
+    
+    });
 
     // Agora faça o PASSO 2 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
 
-    // TESTE 5: Verifique se, ao chamar uma função associada à chave `order` no objeto retornado,
-    // passando uma string como parâmetro (como `objetoRetornado.order('coxinha')`), tal string é adicionada
-    // ao array retornado em `objetoRetornado.consumption`.
-    // ```
-    // const objetoRetornado = createMenu(objetoQualquer);
-    // objetoRetornado.order("coxinha");
-    // objetoRetornado.consumption // Retorno: ["coxinha"]
-    // ```
+    it("Verifica se, ao chamar uma função associada à chave 'order' no objeto retornado, passando uma string como parâmetro (como objetoRetornado.order('coxinha')), tal string é adicionada ao array retornado em 'objetoRetornado.consumption'", () => {
+
+      const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };  
+      const objetoRetornado = createMenu(menu);
+    
+      objetoRetornado.order('coxinha')
+    
+      expect(objetoRetornado.consumption).toEqual(['coxinha']);
+    
+    });
 
     // Agora faça o PASSO 3 no arquivo `src/restaurant.js`.
     // --------------------------------------------------------------------------------------
 
-    // TESTE 6: Verifique se, ao adicionar três pedidos, dentre bebidas e comidas, o array `objetoRetornado.consumption` contém os itens pedidos.
-    // ```
-    // objetoRetornado.order("coxinha");
-    // objetoRetornado.order("agua");
-    // objetoRetornado.order("sopa");
-    // objetoRetornado.order("sashimi");
-    // objetoRetornado.consumption // Retorno: ["coxinha", "agua", "sopa", "sashimi"]
-    // ```
+    it("Verifica se, ao adicionar três pedidos, dentre bebidas e comidas, o array `objetoRetornado.consumption` contém os itens pedidos.", () => {
+    
+      const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };  
+      const objetoRetornado = createMenu(menu);
+      
+      objetoRetornado.order("coxinha");
+      objetoRetornado.order("agua");
+      objetoRetornado.order("sopa");
+      objetoRetornado.order("sashimi");
+
+      expect(objetoRetornado.consumption).toEqual(["coxinha", "agua", "sopa", "sashimi"]);
+    
+    });
 
     // Agora faça o TESTE 7 deste arquivo.
     // --------------------------------------------------------------------------------------
 
-    // TESTE 7: Verifique se a função `order` aceita que pedidos repetidos sejam acrescidos a `consumption`.
-    // ```
-    // objetoRetornado.order('coxinha');
-    // objetoRetornado.order('agua');
-    // objetoRetornado.order('coxinha');
-    // objetoRetornado.consumption // Retorno: ['coxinha', 'agua', 'coxinha']
-    // ```
+    it("Verifica se a função `order` aceita que pedidos repetidos sejam acrescidos a `consumption`", () => {
+      
+      const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };  
+      const objetoRetornado = createMenu(menu);
+      
+      objetoRetornado.order('coxinha');
+      objetoRetornado.order('agua');
+      objetoRetornado.order('coxinha');
 
+      expect(objetoRetornado.consumption).toEqual(["coxinha", "agua", "coxinha"]);
+
+    });
+  
     // Agora faça o TESTE 8 deste arquivo.
     // --------------------------------------------------------------------------------------
 
-    // TESTE 8: Verifique se, ao chamar `objetoRetornado.pay()`, retorna-se a soma dos preços de tudo que foi pedido, conforme registrado em `objetoRetornado.consumption`
-    // ```
-    // objetoRetornado.order('coxinha');
-    // objetoRetornado.order('agua');
-    // objetoRetornado.order('coxinha');
-    // objetoRetornado.pay() // Retorno: somaDosPreçosDosPedidos
-    // ```
-  
+    it("Verifica se, ao chamar 'objetoRetornado.pay()', retorna-se a soma dos preços de tudo que foi pedido, conforme registrado em 'objetoRetornado.consumption'", () => {
+      
+      const menu = { food: {'coxinha': 3.9, 'sopa': 9.9}, drink: {'agua': 3.9, 'cerveja': 6.9} };  
+      const objetoRetornado = createMenu(menu);
+
+      objetoRetornado.order('coxinha');
+      objetoRetornado.order('agua');
+      objetoRetornado.order('coxinha');
+      objetoRetornado.order('sopa');
+
+
+      expect(objetoRetornado.pay()).toBeCloseTo(23.76);
+
+    });
+
     // Agora faça o PASSO 4 no arquivo `src/restaurant.js`.
   });
-});
